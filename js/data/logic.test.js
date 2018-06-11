5aa767dd0e4bc6328d70c0509b3638cea7c88ca7
Grigoriy Levanov <glevanov@gmail.com>
2018-06-11 14:38:55 +0300

    Переписывает тесты для подсчета очков

--- js/data/logic.test.js
+++ js/data/logic.test.js
@@ -1,18 +1,13 @@
 import {assert} from 'chai';
 import {calculateScore} from '../logic.js';
-/*
-Итого тестируем
-Функция подсчёта набранных баллов игрока
-Функция вывода результата игрока
-функция создания таймера
-*/
-function Answer(isCorrect, time) {
-  this.isCorrect = isCorrect;
-  this.time = time;
-}
 
 const getMockAnswers = (totalAnswers, correctAnswers, quickAnswers) => {
-  let answers = [];
+  function Answer(isCorrect, time) {
+    this.isCorrect = isCorrect;
+    this.time = time;
+  }
+
+  const answers = [];
   const longAnswer = 60;
   const quickAnswer = 10;
 
@@ -29,20 +24,23 @@ const getMockAnswers = (totalAnswers, correctAnswers, quickAnswers) => {
 };
 
 describe(`check score calculation`, () => {
-  it(`handles less then 10 answers`, () => {
-    assert.equal(-1, calculateScore(getMockAnswers(9, 9, 9)));
+  it(`fails if less then 10 answers`, () => {
+    assert.equal(-1, calculateScore(getMockAnswers(9, 9, 9), 0));
+    assert.equal(-1, calculateScore(getMockAnswers(9, 8, 1), 1));
+    assert.equal(-1, calculateScore(getMockAnswers(9, 7, 5), 2));
   });
-  it(`handles 3 errors`, () => {
-    assert.equal(0, calculateScore(getMockAnswers(6, 3, 0)));
+  it(`fails once there are 3 notes`, () => {
+    assert.equal(-1, calculateScore(getMockAnswers(6, 3, 0), 3));
+    assert.equal(-1, calculateScore(getMockAnswers(5, 2, 1), 3));
+    assert.equal(-1, calculateScore(getMockAnswers(4, 1, 1), 3));
   });
-  it(`calculates correct answers`, () => {
-    assert.equal(0, calculateScore(getMockAnswers(10, 1, 0)));
-    assert.equal(4, calculateScore(getMockAnswers(10, 8, 0)));
-    assert.equal(10, calculateScore(getMockAnswers(10, 10, 0)));
-    assert.equal(0, calculateScore(getMockAnswers(10, 1, 1)));
-    assert.equal(12, calculateScore(getMockAnswers(10, 8, 8)));
-    assert.equal(20, calculateScore(getMockAnswers(10, 10, 10)));
-    assert.equal(0, calculateScore(getMockAnswers(10, 5, 1)));
-    assert.equal(15, calculateScore(getMockAnswers(10, 10, 5)));
+  it(`calculates score correctly`, () => {
+    assert.equal(10, calculateScore(getMockAnswers(10, 10, 0), 0));
+    assert.equal(20, calculateScore(getMockAnswers(10, 10, 10), 0));
+    assert.equal(7, calculateScore(getMockAnswers(10, 9, 0), 1));
+    assert.equal(12, calculateScore(getMockAnswers(10, 9, 5), 1));
+    assert.equal(4, calculateScore(getMockAnswers(10, 8, 0), 2));
+    assert.equal(5, calculateScore(getMockAnswers(10, 8, 1), 2));
+    assert.equal(12, calculateScore(getMockAnswers(10, 8, 8), 2));
   });
 });
