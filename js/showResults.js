e2449d2523dbfdad14f0dcb4fc270306d4e15825
Grigoriy Levanov <glevanov@gmail.com>
2018-06-11 19:32:00 +0300

    Добавляет функцию вывода результата

--- js/showResults.js
+++ js/showResults.js
@@ -1,1 +1,19 @@
-export const showResults = (otherResults, playerResult) => {};
+export const showResults = (otherResults, playerResult) => {
+  if (playerResult.time <= 0) {
+    return `Время вышло! Вы не успели отгадать все мелодии`;
+  }
+  if (playerResult.notes <= 0) {
+    return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
+  }
+
+  const playerScore = playerResult.score;
+  const statistics = otherResults.slice();
+  statistics.push(playerScore);
+  statistics.sort((a, b) => a - b);
+  const totalPlayers = statistics.length;
+  const scoreIndex = statistics.indexOf(playerScore);
+  const place = totalPlayers - scoreIndex;
+  const playerPercentage = scoreIndex / otherResults.length * 100;
+
+  return `Вы заняли ${place} место из ${totalPlayers} игроков. Это лучше, чем у ${playerPercentage}% игроков`;
+};
